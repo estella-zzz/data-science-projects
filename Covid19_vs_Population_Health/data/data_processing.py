6d2c418b547ee432361a274758393a48438dc49d
Estella Zhang <kaixin_z@hotmail.com>
2020-07-11 12:26:58 -0500

    type:feat Drop unused columns

--- Covid19_vs_Population_Health/data/data_processing.py
+++ Covid19_vs_Population_Health/data/data_processing.py
@@ -2,14 +2,16 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 
-census = pd.read_csv('census-2018.csv')
 covid = pd.read_csv('COVID19_US.csv')
-
 covid.columns = [x.lower() for x in covid.columns]
 covid = covid.loc[covid['country_short_name'] == 'United States']
+covid = covid.drop(columns = ['continent_name','data_source_name','country_alpha_3_code','country_short_name','country_alpha_2_code','county_fips_number'])
 
-covid.iloc[:5,:4]
-
-census.loc[census['placename'] == 'Autagua']
+census = pd.read_csv('census-2018.csv')
+census = census.loc[census['GeographicLevel'] == 'City']
+census = census.drop(columns = ['GeographicLevel','DataSource','UniqueID','Data_Value_Unit','DataValueTypeID',\
+    'Low_Confidence_Limit','High_Confidence_Limit','Geolocation','CategoryID','CityFIPS',
+    'TractFIPS','States','Counties'])
 
 cities = pd.read_csv('uscities.csv')
+cities.drop(columns = ['military','source','incorporated','timezone','ranking','zips','id'])
