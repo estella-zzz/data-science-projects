04b9873fed91a0be3f99632b33440e679402b8de
Estella Zhang <kaixin_z@hotmail.com>
2020-07-19 16:37:57 -0500

    type:docs Clean files for blog

--- Covid19_vs_Population_Health/README.md
+++ Covid19_vs_Population_Health/README.md
@@ -1,7 +1,16 @@
 # Covid 19 vs Population Health
 
-### Data Overview
+### Overview
+This project uses the daily COVID activity data from data.world, the 500 Cities: Local Data for Better Health, 2018 release from CDC and mapping data from simplemaps to study how population health measures impact the COVID-19 spread in U.S cities. Please see the blog post for details.
+
+
+### Data Source
 - Census data: https://catalog.data.gov/dataset/500-cities-local-data-for-better-health-2018-release-7203f
 - Covid data: https://query.data.world/s/vokh24kdzeobd6ltdd6z5oksttdth5
 - City data: https://simplemaps.com/data/us-cities
+
+### Instructions
+1. Run the following commands in the project's 'data' directory to set up your database.
+`python data_processing.py`
+2. Run `explore.ipynb` to see the data discovery and model results.
 
--- Covid19_vs_Population_Health/data/data_processing.py
+++ Covid19_vs_Population_Health/data/data_processing.py
@@ -4,6 +4,7 @@ import numpy as np
 from sqlalchemy import create_engine
 
 ## Retrieve covid data from data.world
+print('loading covid data...')
 covid = pd.read_csv('https://query.data.world/s/vokh24kdzeobd6ltdd6z5oksttdth5')
 
 ## Clean covid data, get case and death on Jun 1st
@@ -12,8 +13,10 @@ covid = covid.loc[covid['country_short_name'] == 'United States']
 covid = covid.drop(columns = ['continent_name','data_source_name','country_alpha_3_code','country_short_name','country_alpha_2_code'])
 covid['report_date'] = pd.to_datetime(covid['report_date'] )
 covid_new = covid.loc[covid['report_date'] == '2020-06-01'][['people_positive_cases_count','county_fips_number','people_death_count']]
+print('covid data successfully loaded')
 
 ## Download census data from CDC and save it locally if file not in local env yet
+print('loading census data...')
 try:
     census = pd.read_csv('census-2018.csv')
 except: 
@@ -28,12 +31,17 @@ measures = census[['MeasureId','Measure','Short_Question_Text','Category']].drop
 census = census.drop(columns = ['Year','GeographicLevel','DataSource','UniqueID','Data_Value_Unit','DataValueTypeID',\
     'Low_Confidence_Limit','High_Confidence_Limit','Geolocation','CategoryID','CityFIPS',
     'TractFIPS','States','Counties','Measure','Short_Question_Text'])
+print('census data successfully loaded')
 
 ## Get city match. Resource: https://simplemaps.com/data/us-cities
+print('loading city mapping data...')
 cities = pd.read_csv('uscities.csv')
 cities = cities.drop(columns = ['military','source','incorporated','timezone','ranking','zips','id'])
+city_pop = cities.groupby('county_fips').agg({'population':'sum'}).reset_index()
+print('mapping data successfully loaded')
 
 ## Get county info for census data
+print('merging datasets...')
 map = census.merge(cities[['city', 'state_id','county_name','county_fips','density']], how = 'inner', left_on = ['CityName','StateAbbr'], right_on = ['city','state_id'])
 map = map.dropna(subset = ['county_name','Data_Value'])
 
@@ -43,28 +51,20 @@ map_new = map.groupby(['CityName','county_fips','state_id','density','MeasureId'
 map_new['Data_Value'] = map_new.apply(lambda x: x['Data_Value']/x['PopulationCount'], axis = 1)
 map_new = map_new.pivot_table(index = ['CityName', 'county_fips', 'state_id','PopulationCount','density'],columns = ['MeasureId'],values = ['Data_Value']).reset_index()
 
+# Clean column names
 index_list = map_new.columns.droplevel(1)[0:5]
 measure_list = map_new.columns.droplevel(0)[5:]
 map_new.columns = index_list.append(measure_list)
 
-## Covert covid data to case/death per 1M population
-city_pop = cities.groupby('county_fips').agg({'population':'sum'}).reset_index()
-covid_new = covid_new.merge(city_pop, left_on = 'county_fips_number', right_on = 'county_fips')
-covid_new['case1m'] = covid_new.apply(lambda x: x['people_positive_cases_count']/x['population'] * 1000000, axis = 1)
-covid_new['death1m'] = covid_new.apply(lambda x: x['people_death_count']/x['population'] * 1000000, axis = 1)
-covid_new = covid_new.drop(columns = ['county_fips','population','people_positive_cases_count','people_death_count'])
-
-## Merge datasets
-output = map_new.merge(covid_new, how = 'inner', left_on = 'county_fips', right_on = 'county_fips_number')
-output = output.drop(columns = ['county_fips_number','PopulationCount'])
-
-##100d cases/death
+print('calculating covid metrics...')
+## Get cases/deaths 100 days since the first case
 fcd = covid.loc[covid['people_positive_cases_count']>0].groupby('county_fips_number').agg({'report_date':'min'}).reset_index()
 fcd = fcd.rename(columns = {'report_date':'first_date'})
 covid_100d = covid.merge(fcd, how = 'inner', on = 'county_fips_number')
 covid_100d['days_since'] = covid_100d.apply(lambda x: (x['report_date'] - x['first_date']).days, axis = 1)
 covid_100d = covid_100d.loc[covid_100d['days_since'] == 100][['people_positive_cases_count','county_fips_number','people_death_count', 'first_date']]
 
+## Covert covid data to case/death per 1M population
 covid_100d = covid_100d.merge(city_pop, left_on = 'county_fips_number', right_on = 'county_fips')
 covid_100d['case1m'] = covid_100d.apply(lambda x: x['people_positive_cases_count']/x['population'] * 1000000, axis = 1)
 covid_100d['death1m'] = covid_100d.apply(lambda x: x['people_death_count']/x['population'] * 1000000, axis = 1)
@@ -73,6 +73,7 @@ covid_100d = covid_100d.drop(columns = ['county_fips','population','people_posit
 output_100 = map_new.merge(covid_100d, how = 'inner', left_on = 'county_fips', right_on = 'county_fips_number')
 output_100 = output_100.drop(columns = ['county_fips_number','PopulationCount'])
 
+## Get 14d case growth
 covid14d = covid.merge(fcd, how = 'inner', on = 'county_fips_number')
 covid14d['days_since'] = covid14d.apply(lambda x: (x['report_date'] - x['first_date']).days, axis = 1)
 covid14d = covid14d.loc[covid14d['days_since'] == 86][['people_positive_cases_count','county_fips_number','people_death_count', 'first_date']]
@@ -86,8 +87,10 @@ output_100['case14'] = output_100['case1m'] - output_100['case14']
 output_100['death14'] = output_100['death1m'] - output_100['death14']
 
 ## Save to coviddb
+print('saving data to coviddb...')
 engine = create_engine('sqlite:///%s' % 'coviddb')
-output.to_sql('CovidData', engine, index=False, if_exists='replace')
-measures.to_sql('MeasureLookup', engine, index=False, if_exists='replace')
 
+measures.to_sql('MeasureLookup', engine, index=False, if_exists='replace')
 output_100.to_sql('Covid100', engine, index=False, if_exists='replace')
+
+print('data loading completed. have fun!')
